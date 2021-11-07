e5697a9e0d712d976c9fa5cf0b6f1942f84c50b9
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2021-11-07 00:51:45 -0700

    JavaScript file revised

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ function removesDefinition(element){
     element.remove();
 }
 
+var countA = 13;
+var countB = 37;
+
 function likes(id){
-    var likesTally = document.getElementById(id);
+  var likesTally = document.getElementById(id);
+  if(id=="likesTop"){
     window.alert("Ninja was liked");
-    // likesTally.innerHTML++;
+    countA++;
+    likesTally.innerText = countA + " likes";
+  }else{
+    window.alert("Ninja was liked");
+    countB++;
+    likesTally.innerText = countB + " likes";
+    }
 }
+
