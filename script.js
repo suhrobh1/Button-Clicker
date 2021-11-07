80bd7c99a7a76f3b312d1e50f1249f839f315542
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2021-11-06 21:51:58 -0700

    Formatting changes

---
+++ script.js
@@ -0,0 +1,13 @@
+function loginChange(element){
+    element.innerHTML= "Logout";
+}
+
+function removesDefinition(element){
+    element.remove();
+}
+
+function likes(id){
+    var likesTally = document.getElementById(id);
+    window.alert("Ninja was liked");
+    likesTally.innerHTML++;
+}
